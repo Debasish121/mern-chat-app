8ade47fce3a6e556bc965721959274b86679999d
Debasish Vishal <debav2@gmail.com>
2024-06-27 22:04:10 +0530

    Updated

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import useSignup from "../../hooks/useSignup";
-import GenderCheckbox from "./GenderCheckbox.js";
+import GenderCheckbox from "./GenderCheckbox.jsx";
 
 const SignUp = () => {
   const [inputs, setInputs] = useState({
