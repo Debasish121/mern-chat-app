7632cec1c5893a35ce4c473d2eeace1054bc0cf1
Debasish Vishal <debav2@gmail.com>
2024-06-27 16:26:33 +0530

    Completed frontend logic

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { BiLogOut } from "react-icons/bi";
+import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
+  const { loading, logout } = useLogout();
+
   return (
-    <div className="mt-6">
-      <BiLogOut className="w-6 h-6 text-white cursor-pointer" />
+    <div className="mt-2">
+      {!loading ? (
+        <BiLogOut
+          className="w-6 h-6 text-white cursor-pointer"
+          onClick={logout}
+        />
+      ) : (
+        <span className="loading loading-spinner"></span>
+      )}
     </div>
   );
 };
---
+++ frontend/src/zustand/useConversation.js
@@ -0,0 +1,12 @@
+// Zustand is a state management solution designed to be small, fast, and scalable
+import { create } from "zustand";
+
+const useConversation = create((set) => ({
+  selectedConversation: null,
+  setSelectedConversation: (selectedConversation) =>
+    set({ selectedConversation }),
+  messages: [],
+  setMessages: (messages) => set({ messages }),
+}));
+
+export default useConversation;
